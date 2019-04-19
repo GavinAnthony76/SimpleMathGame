bc8fd3e336ddb4d3080e9e8c3e2ccae7b0471e3b
gavin anthony <guidato.llc@gmail.com>
2019-04-19 15:32:41 -0500

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace SimpleMathGame
+{
+    public class Calculator
+    {
+        public static void addition()
+        {
+            var numberGenerator = new Random(); var number1 = numberGenerator.Next(1, 11); var number2 = numberGenerator.Next(1, 11); Console.WriteLine("What is the first number plus the second number? " + number1 + " + " + number2 + " ="); var answer = int.Parse(Console.ReadLine()); if (answer == number1 + number2)
+            { Console.WriteLine("That is Correct! Press ENTER"); Console.ReadKey(); }
+            else
+            { Console.WriteLine("That is not correct! Press ENTER"); Console.ReadKey(); }
+        }
+        public static void subtraction()
+        {
+            var numberGenerator = new Random(); var number1 = numberGenerator.Next(1, 11); var number2 = numberGenerator.Next(1, 11); Console.WriteLine("What is the first number minus the second number? " + number1 + " - " + number2 + " ="); var answer = int.Parse(Console.ReadLine()); if (answer == number1 - number2)
+            { Console.WriteLine("That is Correct! Press ENTER "); Console.ReadKey(); }
+            else
+            { Console.WriteLine("That is not correct! Press ENTER"); Console.ReadKey(); }
+        }
+        public static void multiplication()
+        {
+            var numberGenerator = new Random(); var number1 = numberGenerator.Next(1, 11); var number2 = numberGenerator.Next(1, 11); Console.WriteLine("What is the first number times the second number? " + number1 + " * " + number2 + " ="); var answer = int.Parse(Console.ReadLine()); if (answer == number1 * number2)
+            { Console.WriteLine("That is Correct! Press ENTER"); Console.ReadKey(); }
+            else
+            { Console.WriteLine("That is not correct! Press ENTER"); Console.ReadKey(); }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SimpleMathGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello and welcome to simple math.\n Press ENTER to continue. ");
+          
+            Console.ReadKey();
+
+
+            var numberGenerator = new Random();
+            var repeat = true;
+
+            while (repeat)
+            {
+             
+                var choice = numberGenerator.Next(1, 4);
+
+                if (choice == 1)
+                   Calculator.subtraction();
+                else if (choice == 2)
+                   Calculator.addition();
+                else if (choice == 3)
+                   Calculator.multiplication();
+                Console.Write("Do you wish to continue? Enter Y for yes or N for no, then press ENTER ");
+               
+                string selection;
+                selection = Console.ReadLine();
+
+                if (selection == "Y" || selection == "y")
+                {
+                    repeat = true;
+                }
+                else if (selection == "N" || selection == "n")
+                {
+                    repeat = false;
+                    Console.WriteLine("Thanks for playing");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
